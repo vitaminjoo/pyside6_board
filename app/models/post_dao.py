908a2b512c49847841a5a8cb129e6393212d03bf
vitaminjoo <goatjswn.work@gmail.com>
2025-12-22 22:36:38 +0900

    Project Init: PySide6 환경설정, 패키지 구조 설정, DB 매니저 작성

---
+++ app/models/post_dao.py
@@ -0,0 +1,61 @@
+from app.database import db
+
+class PostDao:
+    """
+    게시글(Posts)과 관련한 DB 작업을 전담하는 클래스입니다.
+    SQL 쿼리는 이 파일 안에만 존재해야 합니다.
+    """
+
+    def insert_post(self, title, content, author="anonymous"):
+        conn = db.get_connection()
+        cursor = conn.cursor()
+
+        sql = "INSERT INTO posts (title, content, author) VALUES (?, ?, ?)"
+        cursor.execute(sql, (title, content, author))
+
+        conn.commit()
+        conn.close()
+        print(f"글 작성 완료: {title}")
+
+    def get_all_posts(self):
+        conn = db.get_connection()
+        cursor = conn.cursor()
+
+        sql = "SELECT * FROM posts ORDER BY created_at DESC"
+        cursor.execute(sql)
+
+        rows = cursor.fetchall()
+
+        conn.close()
+        return rows
+
+    def get_post(self, id):
+        conn = db.get_connection()
+        cursor = conn.cursor()
+
+        sql = "SELECT * FROM posts WHERE id = ?"
+        cursor.execute(sql, (id,))
+        row = cursor.fetchone()
+        conn.close()
+        return row
+
+    def update_post(self, id, title, content, author="anonymous"):
+        conn = db.get_connection()
+        cursor = conn.cursor()
+
+        sql = "UPDATE posts SET title=?, content=? WHERE id = ?"
+        cursor.execute(sql, (title, content, id))
+        conn.commit()
+        conn.close()
+        print(f"수정 완료: ID: {id}")
+
+    def delete_post(self, id):
+        conn = db.get_connection()
+        cursor = conn.cursor()
+
+        sql = "DELETE FROM posts WHERE id = ?"
+        cursor.execute(sql, (id,))
+        conn.commit()
+        conn.close()
+        print(f"삭제 완료: id: {id}")
+
